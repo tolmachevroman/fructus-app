bf0ec771df14f6f71436df2f7a8352e5c821891d
Roman Tolmachev <tolmachevroman@gmail.com>
2023-12-15 08:02:40 -0300

    initial project setup

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Roman Tolmachev on 15.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
